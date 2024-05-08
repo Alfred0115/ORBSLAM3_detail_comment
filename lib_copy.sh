3a0d1988c5706522e8ab6beb107dae72fb818faf
Alfred0115 <li_zhqin@163.com>
2024-05-08 16:48:50 +0800

    complier

---
+++ lib_copy.sh
@@ -0,0 +1,7 @@
+cd ~
+mkdir run_lib
+cd /home/li/catkin_ws/src/ORB_SLAM3_detailed_comments
+cp Thirdparty/g2o/lib/libg2o.so         ~/run_lib
+cp Thirdparty/DBoW2/lib/libDBoW2.so     ~/run_lib
+cp lib/libORB_SLAM3.so                  ~/run_lib
+cp Thirdparty/Pangolin/build/*.so     ~/run_lib
